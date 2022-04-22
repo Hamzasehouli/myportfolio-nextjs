a0758214d31c8eada39a9636e0b8fb70a24fa0ea
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-22 20:34:27 +0000

    Added splash page into the _app comp

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useLayoutEffect, useState } from "react";
+import Image from "next/image";
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+  const [start, setStart] = useState(
+    <div className="splash">
+      <Image width={100} height={100} src="/Logo.svg" />
+    </div>
   );
+
+  setTimeout(() => {
+    setStart(
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }, 3000);
+  // useLayoutEffect(() => {}, [start]);
+
+  return start;
 }
 
 export default MyApp;
