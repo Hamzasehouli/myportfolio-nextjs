3c924a8b77bc6dda9599505904f0ea24c515fc2a
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-22 18:53:33 +0000

    Changed html img tag with Image comp

--- components/MainNav.js
+++ components/MainNav.js
@@ -2,13 +2,19 @@ import Class from "./MainNav.module.css";
 import Link from "next/Link";
 import { useRouter } from "next/router";
 import BaseButton from "./BaseButton";
-
+import Image from "next/image";
 const MainNav = function (props) {
   const router = useRouter();
   return (
     <nav className={Class.nav}>
       <Link href="/">
-        <img className="logo" src="/Logo.svg" />
+        <Image
+          alt="Hamza Sehouli portfolio logo"
+          width={70}
+          height={70}
+          className="logo"
+          src="/Logo.svg"
+        />
       </Link>
       <ul className={Class.list}>
         <li>
