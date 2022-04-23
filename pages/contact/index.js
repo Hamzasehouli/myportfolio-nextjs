13d11592d726a0642112a759106a5a347f8a20b6
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-23 12:26:08 +0000

    Added a paragraph in contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,6 +4,10 @@ import BaseForm from "../../components/BaseForm";
 const Contact = function () {
   return (
     <BasePage title="Contact">
+      <p style={{ width: "60%", marginTop: "3rem" }}>
+        I am currenty looking for new job opportunities. However if you have a
+        request or question, feel free to use this form.
+      </p>
       <BaseForm />
     </BasePage>
   );
