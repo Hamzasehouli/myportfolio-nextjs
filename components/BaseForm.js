6c59c35018879efe0241937446a7da5773d7fcf2
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-23 11:49:57 +0000

    Finished BaseForm comp

--- components/BaseForm.js
+++ components/BaseForm.js
@@ -1,21 +1,25 @@
-import BaseButton from "../../components/BaseButton";
+import BaseButton from "./BaseButton";
 import Classes from "./BaseForm.module.css";
 const BaseForm = function () {
-  <form className={Classes.form}>
-    <div>
-      <input placeholder="Name" />
-      <input placeholder="Email" />
-    </div>
-    <div>
-      <input placeholder="Subject" />
-    </div>
-    <div>
-      <textarea placeholder="Message"></textarea>
-    </div>
-    <BaseButton mode="button" type="submit" state="empty">
-      Send
-    </BaseButton>
-  </form>;
+  return (
+    <form className={Classes.form}>
+      <div className={Classes.entry}>
+        <input type="text" className={Classes.input} placeholder="Name" />
+        <input type={"email"} className={Classes.input} placeholder="Email" />
+      </div>
+      <div className={Classes.entry}>
+        <input type="text" className={Classes.input} placeholder="Subject" />
+      </div>
+      <div className={Classes.entry}>
+        <textarea className={Classes.textarea} placeholder="Message"></textarea>
+      </div>
+      <div className={Classes.btn}>
+        <BaseButton mode="button" type="submit" state="empty">
+          Send email
+        </BaseButton>
+      </div>
+    </form>
+  );
 };
 
 export default BaseForm;
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,12 @@
 import BasePage from "../../components/BasePage";
+import BaseForm from "../../components/BaseForm";
 
 const Contact = function () {
-  return <BasePage title="Contact"></BasePage>;
+  return (
+    <BasePage title="Contact">
+      <BaseForm />
+    </BasePage>
+  );
 };
 
 export default Contact;
