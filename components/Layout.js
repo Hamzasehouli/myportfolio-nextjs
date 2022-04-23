8ad09d0ec31dd35264f8bf9a8b42ec88dabdc185
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-23 20:57:12 +0000

    Added links to the side bar social links

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,7 @@ const Layout = function (props) {
         </BaseSection>
         <ul className="icons">
           <li>
-            <a href="#">
+            <a target="_blank" href="https://github.com/Hamzasehouli">
               <svg className="icon">
                 <use href="/sprite.svg#icon-github"></use>
               </svg>
@@ -29,35 +29,55 @@ const Layout = function (props) {
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://hub.docker.com/u/hamzasehouli"
+            >
               <svg className="icon">
                 <use href="/sprite.svg#icon-docker"></use>
               </svg>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://codepen.io/sh-hamza"
+            >
               <svg className="icon">
                 <use href="/sprite.svg#icon-codepen"></use>
               </svg>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/hamzasehouli/"
+            >
               <svg className="icon">
                 <use href="/sprite.svg#linkedin"></use>
               </svg>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://medium.com/@sehouli.hamza"
+            >
               <svg className="icon">
                 <use href="/sprite.svg#icon-medium"></use>
               </svg>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.youtube.com/channel/UC6ROqFB77uZr14lwMm0CmAw"
+            >
               <svg className="icon">
                 <use href="/sprite.svg#icon-youtube"></use>
               </svg>
