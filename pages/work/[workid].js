4cb1f6eef81218fa357943c194ee0fb7a4018ce0
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-24 21:15:00 +0000

    Deleted [workid] page

--- pages/work/[workid].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-const WorkEelement = function () {
-  const router = useRouter();
-  console.log(router);
-  return <div>element</div>;
-};
-
-export default WorkEelement;
