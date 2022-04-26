b7aecf865a408fd1c0ff9825c5255aaf844b3b3e
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-26 16:04:08 +0000

    Finished the mainNavbar and home page responsiveness

--- components/MainNav.js
+++ components/MainNav.js
@@ -38,9 +38,46 @@ const MainNav = function (props) {
           </BaseButton>
         </li>
       </ul>
-      <div className={Class.hamburger}>
+      <input id="hidden" className={Class["input-hidden"]} type="checkbox" />
+      <label for="hidden" className={Class.hamburger}>
         <span className={Class["hamburger_unity"]}></span>
-      </div>
+      </label>
+      <div className={Class.blur}></div>
+      <ul className={Class["list-responsive"]}>
+        <li>
+          <BaseButton mode="anchor" state="ghost" to="/about">
+            About
+          </BaseButton>
+        </li>
+        <li>
+          <BaseButton mode="anchor" state="ghost" to="/work">
+            Work
+          </BaseButton>
+        </li>
+        <li>
+          <BaseButton mode="anchor" state="ghost" to="/contact">
+            Contact
+          </BaseButton>
+        </li>
+        <li>
+          <BaseButton to="/cv-hamza-sehouli.pdf" mode="standard" state="ghost">
+            Resume
+          </BaseButton>
+        </li>
+        <li>
+          <Link href="/" passHref>
+            <a>
+              <Image
+                alt="Hamza Sehouli portfolio logo"
+                width={70}
+                height={70}
+                className="logo-responsive"
+                src="/Logo.svg"
+              />
+            </a>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
